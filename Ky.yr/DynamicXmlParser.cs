656576bafb89f54047bfa8e2dc0318f1226dd0f9
Kjetil Barnung <kjetil.barnung@gmail.com>
2012-06-15 10:40:50 +0200

    Initial commit of file structure
    Solution file included, but all referenced projects is not

---
+++ Ky.yr/DynamicXmlParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Dynamic;
+using System.Xml.Linq;
+using System.IO;
+
+namespace Ky.yr
+{
+
+    /// <summary>
+    /// Takes a XML string as input param and returns a dynamic object that you can just type out the stuff you want.
+    /// Like you do in umbraco Razor code
+    /// </summary>
+    public class DynamicXml : DynamicObject, IEnumerable
+    {
+        private readonly List<XElement> _elements;
+
+        public DynamicXml(string text)
+        {
+            var doc = XDocument.Parse(text);
+            _elements = new List<XElement> { doc.Root };
+        }
+
+        protected DynamicXml(XElement element)
+        {
+            _elements = new List<XElement> { element };
+        }
+
+        protected DynamicXml(IEnumerable<XElement> elements)
+        {
+            _elements = new List<XElement>(elements);
+        }
+
+        public override bool TryGetMember(
+            GetMemberBinder binder, out object result)
+        {
+            result = null;
+            if (binder.Name == "Value")
+                result = _elements[0].Value;
+            else if (binder.Name == "Count")
+                result = _elements.Count;
+            else
+            {
+                var attr = _elements[0].Attribute(
+                    XName.Get(binder.Name));
+                if (attr != null)
+                    result = attr;
+                else
+                {
+                    var items = _elements.Descendants(
+                        XName.Get(binder.Name));
+                    if (items == null || items.Count() == 0)
+                        return false;
+                    result = new DynamicXml(items);
+                }
+            }
+            return true;
+        }
+
+        public override bool TryGetIndex(GetIndexBinder binder,
+            object[] indexes, out object result)
+        {
+            int ndx = (int)indexes[0];
+            result = new DynamicXml(_elements[ndx]);
+            return true;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var element in _elements)
+                yield return new DynamicXml(element);
+        }
+    }
+    
+}
---
+++ Ky.yr/WeatherHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Web;
+using System.Runtime.Caching;
+using System.Xml.Serialization;
+
+namespace Ky.yr
+{
+    public class WeatherHandler
+    {
+        private static WebClient _client;
+        private static ObjectCache _cache;
+
+        private WeatherHandler()
+        {
+            _client = _client ?? new WebClient();
+            _client.Encoding = Encoding.UTF8;
+            _cache = _cache ?? MemoryCache.Default;
+            
+
+        }
+        private string GetData(string url)
+        {
+            var result = string.Empty;
+
+            try
+            {
+
+                // try getting the item from the short term cache
+                result = (string) _cache.Get(url);
+
+                // if short term cache does not return a result for this uri, load it from finn.no
+                if (string.IsNullOrEmpty(result))
+                {
+                    result = _client.DownloadString(url);
+
+                    // new content was downloaded, update the local cache stores for this item.
+                    _cache.Set(url, result, new DateTimeOffset(DateTime.Now.AddMinutes(15)));
+
+                    // add a long time cache store, to use in case of service outage from finn.no
+                    _cache.Set("lt:" + url, result, new DateTimeOffset(DateTime.Now.AddDays(1)));
+                }
+            }
+            catch (Exception e)
+            {
+                // if the web client fails to download content from finn.no try looking in the long time cache store to serve content to the user during the outage
+                result = _cache.Get("lt:" + url) as string ?? string.Empty;
+            }
+            return result;
+        }
+        public static DynamicXml GetForeceast(string url)
+        {
+            WeatherHandler _me = new WeatherHandler();
+            string result = _me.GetData(url);
+            return new DynamicXml(result);
+        }
+        
+    }
+}
